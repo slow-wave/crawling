424de620e97f6e4f9ab5cc500a632bbd786fa492
minji <42460302+memoon-mj@users.noreply.github.com>
2021-04-05 01:50:51 +0900

    change

---
+++ crawl_/ex.py
@@ -0,0 +1,12 @@
+import os
+
+print(__file__)
+path = os.path.abspath(__file__)
+print(path)
+print(os.getcwd())
+#print(path+'/'+'category_count_20210330')
+#print(os.path.abspath(__file__))
+
+#print(os.path.dirname(os.path.abspath(__file__)))
+
+#print(os.getcwd())
